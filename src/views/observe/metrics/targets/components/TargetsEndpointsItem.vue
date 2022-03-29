c64ff87ee60ada708abce2c998e96d6b95474dea
sunlintong <sunlintongshuai@gmail.com>
2022-03-29 10:35:26 +0800

    拉取间隔 -> 采集间隔

--- src/views/observe/metrics/targets/components/TargetsEndpointsItem.vue
+++ src/views/observe/metrics/targets/components/TargetsEndpointsItem.vue
@@ -30,7 +30,7 @@
           <v-list-item two-line>
             <v-list-item-content class="py-0">
               <v-list-item-title class="text-subtitle-2 py-1 primary--text">
-                拉取间隔
+                采集间隔
               </v-list-item-title>
               <v-list-item-subtitle class="text-body-2 py-1">
                 {{ endpoint.interval || '-' }}
