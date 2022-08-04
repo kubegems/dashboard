3d9ae3bef76bb0f6aef46d0aec3204eb5b3076c0
leon.chen <imchensl@163.com>
2022-08-04 17:46:51 +0800

    style: :art: code format

--- src/views/resource/environment/components/EnvironmentBaseForm/index.vue
+++ src/views/resource/environment/components/EnvironmentBaseForm/index.vue
@@ -161,7 +161,7 @@
                       prepend-inner-icon="mdi-magnify"
                       @keyup="onAllUsernameInput"
                     />
-                    <v-list dense height="450px" style="overflow-y: auto;">
+                    <v-list dense height="450px" style="overflow-y: auto">
                       <v-list-item v-for="(user, index) in allUsers" :key="index" link @click="setRole(user, index)">
                         <v-list-item-avatar class="my-1">
                           <v-avatar class="white--text font-weight-medium" color="primary" :size="32">
@@ -190,7 +190,7 @@
                       prepend-inner-icon="mdi-magnify"
                       @keyup="onRoleUsernameInput"
                     />
-                    <v-list dense height="450px" style="overflow-y: auto;">
+                    <v-list dense height="450px" style="overflow-y: auto">
                       <v-list-item
                         v-for="(user, index) in tab === 0 ? readerUsers : operatorUsers"
                         :key="index"
