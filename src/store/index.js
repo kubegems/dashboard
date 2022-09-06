36de4209bed10d8aa4ba38e2cf96c6ea0461d51c
leon.chen <imchensl@163.com>
2022-09-06 13:52:08 +0800

    fix: :bug: fix api-resource mapping bug

--- src/store/index.js
+++ src/store/index.js
@@ -375,9 +375,14 @@ export default new Store({
       const data = await getRESTMapping(payload.clusterName);
       const resource = {};
       data.forEach((d) => {
-        d?.resources?.forEach((r) => {
-          resource[r.kind.toLocaleLowerCase()] = d.groupVersion;
-        });
+        if (
+          !d?.groupVersion.startsWith('apps.kruise.io') &&
+          !d?.groupVersion.startsWith('networking.internal.knative.dev')
+        ) {
+          d?.resources?.forEach((r) => {
+            resource[r.kind.toLocaleLowerCase()] = d.groupVersion;
+          });
+        }
       });
       commit('SET_API_RESOURCES', resource);
     },
